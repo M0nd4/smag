fd94fbe5ec3133b4a3402590f5610c16bdf24d41
vsilv <vsilverlord@googlemail.com>
2014-02-23 17:07:37 +0100

    B2

--- week2/homework/B2.py
+++ week2/homework/B2.py
@@ -4,5 +4,6 @@ N = 4
 p_accept = 0.51516 
 Z = V * p_accept
 Q = (0.2)**2
-P = (Q/Z)**N * 24
-print(P)
+P1 = Q**N * 24
+P2 = (Q/Z)**N * 24
+print("sigma = 0: %.5f and with sigma = 0.1: %.5f "%(P1,P2))
